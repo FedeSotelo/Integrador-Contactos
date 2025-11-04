12df82f5501b09bd16d30ea63fad8281524b47bd
Your Name <lsubotovsky@uade.edu.ar>
2025-11-03 21:13:42 -0300

    pytest2

--- Test.py
+++ Test.py
@@ -15,21 +15,14 @@ def test_validar_correo(correo, esperado):
     assert validar_correo(correo) == esperado
 
 def test_eliminar_grupo_inexistente():
-    grupos_dict.clear()
+    grupos_dict.clear()  
 
-    try:
+    with pytest.raises(LookupError):
         eliminar_grupo()
-        assert False
-    except LookupError:
-        assert True
-
+        
 def test_dias_en_mes():
-    # Meses normales
     assert _dias_en_mes(1, 2024) == 31    
     assert _dias_en_mes(9, 2024) == 30   
-    # Febrero bisiesto
     assert _dias_en_mes(2, 2020) == 29
-    # Febrero NO bisiesto
     assert _dias_en_mes(2, 2021) == 28
-    # Mes inválido
     assert _dias_en_mes(13, 2024) == 0
