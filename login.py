0d79d58a939023f31d0ba211a06f0e82a85284b7
Federico Sotelo <fsotelo@uade.edu.ar>
2025-11-07 20:19:34 -0300

    cambios finales 1.2

--- login.py
+++ login.py
@@ -1,16 +1,18 @@
 import pwinput
 
 def login():
-    usuario_correcto = "Lionel Messi"
-    password_correcto = "messi10"
+    usuarios = {
+        "lionel messi": "messi10",
+        "cristiano ronaldo": "cr7siuu"
+    }
 
-    while True: 
+    while True:
         print("=== Login ===")
         usuario = input("Usuario: ").strip().lower()
         password = pwinput.pwinput("Contraseña: ", mask="*")
 
-        if usuario == usuario_correcto.lower() and password == password_correcto:
-            print("Bienvenido Lio 10")
-            return True   
+        if usuario in usuarios and password == usuarios[usuario]:
+            print(f"Bienvenido {usuario.title()}!")
+            return True
         else:
             print("Usuario o contraseña incorrectos. Intente de nuevo.\n")
